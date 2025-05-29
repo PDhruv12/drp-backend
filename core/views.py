438861e2808f6f69671b453f32f435a34fe4708c
Dhruv <dhruvsinghvi12@gmail.com>
2025-05-29 19:37:06 +0100

    Fix: Fixes the serializers import in views.

--- core/views.py
+++ core/views.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import ExampleTableModel
-from serializers import ExampleTableModelSerializer
+from .serializers import ExampleTableModelSerializer
 
 def my_api_endpoint(request):
   data = {'message': 'Hello from Django!'}
